00ea9b038d0d3a566a154c515a0ff53e6c214b24
Arthur <lotuslovesurmom@gmail.com>
2025-12-28 16:53:24 +0100

    Jump added, some extra physics

---
+++ Assets/Scripts/Player/Jump.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private float verticalVelocity;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -2,34 +2,107 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 5f;
-    [SerializeField] private float _currentSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        _currentSpeed = _moveSpeed;
-    }
+    [Header("Speed")]
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float sprintSpeed = 7f;
+    [SerializeField] private float acceleration = 12f;
+    [SerializeField] private float deceleration = 6f;
 
-    // Update is called once per frame
+    [Header("Collision")]
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private float wallBounceForce = 2f;
+    [SerializeField] private float wallInputLockTime = 0.15f;
+
+    [Header("Direction Lock")]
+    [Range(0f, 1f)]
+    [SerializeField] private float directionLockThreshold = 0.5f;
+
+    [SerializeField] private float currentSpeed;
+    private float moveInput;
+    private int facingDirection = 1;
+
+    private bool isDecelerating;
+    private float inputLockTimer;
+
     void Update()
     {
-       float move =  Input.GetAxis("Horizontal") * _moveSpeed;
-       transform.Translate(move * Time.deltaTime, 0, 0);
-
-
-        switch (move) 
+        // Handle input lock after wall hit
+        if (inputLockTimer > 0f)
         {
-            case > 0:
-                transform.localScale = new Vector3(1, 1, 1);
-                break;
-            case < 0:
-                transform.localScale = new Vector3(-1, 1, 1);
-                break;
+            inputLockTimer -= Time.deltaTime;
+            moveInput = 0;
         }
 
-        if (_currentSpeed < 0.1f)
-       {
-            _currentSpeed = 0;
-       }
+        float rawInput = Input.GetAxisRaw("Horizontal");
+        bool sprintHeld = Input.GetKey(KeyCode.LeftShift);
+
+        if (inputLockTimer <= 0f)
+        {
+            if (rawInput != 0)
+            {
+                int inputDir = (int)Mathf.Sign(rawInput);
+                float speedPercent = Mathf.Abs(currentSpeed) / sprintSpeed;
+
+                if (inputDir != facingDirection && speedPercent >= directionLockThreshold)
+                {
+                    isDecelerating = true;
+                    moveInput = facingDirection; 
+                }
+                else
+                {
+                    moveInput = rawInput;
+                    facingDirection = inputDir;
+                }
+            }
+            else
+            {
+                moveInput = 0;
+            }
+        }
+
+        if (!sprintHeld && Mathf.Abs(currentSpeed) > walkSpeed)
+        {
+            isDecelerating = true;
+        }
+
+        if (Mathf.Abs(currentSpeed) <= walkSpeed)
+        {
+            isDecelerating = false;
+        }
+
+        float targetSpeed = 0f;
+
+        if (moveInput != 0)
+        {
+            targetSpeed = sprintHeld ? sprintSpeed : walkSpeed;
+        }
+
+        float accelRate = isDecelerating ? deceleration : acceleration;
+
+        currentSpeed = Mathf.MoveTowards(
+            currentSpeed,
+            targetSpeed,
+            accelRate * Time.deltaTime
+        );
+
+        transform.Translate(moveInput * currentSpeed * Time.deltaTime, 0, 0);
+
+        HandleFlip();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & wallLayer) != 0)
+        {
+            currentSpeed = wallBounceForce;
+            moveInput = -facingDirection;
+            inputLockTimer = wallInputLockTime;
+            isDecelerating = false;
+        }
+    }
+
+    private void HandleFlip()
+    {
+        transform.localScale = new Vector3(facingDirection, 1, 1);
     }
 }
